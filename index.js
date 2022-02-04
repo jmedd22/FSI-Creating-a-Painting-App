7adde9cc0381512d816198a99589f1c42c499776
Justin Meddaugh <NdoeMusic@Justins-MacBook-Pro.local>
2022-02-04 17:00:54 -0600

    started and got it to print blue

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+let selectedColor = 'blue'
 const painting = document.querySelector('.painting')
 
 painting.addEventListener('click', function(e) {
-    e.target.style.backgroundColor = currentColor
-}
+    e.target.style.backgroundColor = selectedColor
+
+})
+
+// colorChoice.addEventListener('click', function(f) {
+//    f.target.querySelector('color-choice')
+// })
